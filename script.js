6d458db97a87184165a8b8934c2a91b4e21b3632
santoshreddyj <106167286+santoshreddyj@users.noreply.github.com>
2023-05-22 21:56:54 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '29ee9ab8c0msheb4c665ced760fbp141893jsn156287832222',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+//The Headers of Fetch API allows to perform various actions on HTTP request and response headers
+const getweather=(city) =>{
+    cityName.innerHTML= city
+
+
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' +city, options)
+
+    .then(response => response.json())
+    .then((response) => {
+
+        console.log(response)
+
+        cloud_pct.innerHTML = response.cloud_pct
+        temp.innerHTML = response.temp
+        temp2.innerHTML = response.temp
+        feels_like.innerHTML = response.feels_like
+        humidity.innerHTML = response.humidity
+        humidity2.innerHTML = response.humidity
+        min_temp.innerHTML = response.min_temp
+        max_temp.innerHTML = response.max_temp
+        wind_speed.innerHTML = response.wind_speed
+        wind_speed2.innerHTML = response.wind_speed
+        wind_degrees.innerHTML = response.wind_degrees
+        sunrise.innerHTML = response.sunrise
+        sunset.innerHTML=response.sunset 
+
+
+    })
+    .catch(err => console.error(err));
+
+}
+submit.addEventListener("click",(e)=>{
+    e.preventDefault()
+    getweather(city.value)
+})
+getweather("Bangalore")
